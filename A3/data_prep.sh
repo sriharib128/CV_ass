31bc8327c60650c80779f9f0f55db4e57e64e1d8
Srihari <sriharib128>
2024-03-27 22:18:38 +0530

    A3 initial_commit

---
+++ A3/data_prep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Download the video using youtube-dl
+VIDEO_URL="https://www.youtube.com/watch?v=bSMxl1V8FSg"
+youtube-dl -f 'best[height<=480]' -o video.mp4 "$VIDEO_URL"
+
+# Extract frames using ffmpeg
+mkdir frames
+ffmpeg -i video.mp4 -vf fps=25 -t 30 frames/frame_%04d.jpg
+
+# Count the number of frames
+FRAME_COUNT=$(ls frames | wc -l)
+echo "Total frames extracted: $FRAME_COUNT"
+
